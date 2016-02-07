59971e1fcd5be477a489e1904c9e482940f9f617
Rob Clevenger <rob@spring-2885.org>
2016-02-06 17:17:41 -0800

    Added a JSONish error controller to handle all errors. It's not sufficient yet, but at least gives is a JSON type error response page.

---
+++ server/src/main/java/org/spring2885/server/Spring2885ErrorController.java
@@ -0,0 +1,51 @@
+package org.spring2885.server;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@RestController
+@RequestMapping("/error")
+public class Spring2885ErrorController implements ErrorController {
+
+	private final ErrorAttributes errorAttributes;
+
+	@Autowired
+	public Spring2885ErrorController(ErrorAttributes errorAttributes) {
+		Assert.notNull(errorAttributes, "ErrorAttributes must not be null");
+		this.errorAttributes = errorAttributes;
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+
+	@RequestMapping
+	public Map<String, Object> error(HttpServletRequest req) {
+		Map<String, Object> body = getErrorAttributes(req, getTraceParameter(req));
+		return body;
+	}
+
+	private boolean getTraceParameter(HttpServletRequest request) {
+		String parameter = request.getParameter("trace");
+		if (parameter == null) {
+			return false;
+		}
+		return !"false".equals(parameter.toLowerCase());
+	}
+
+	private Map<String, Object> getErrorAttributes(HttpServletRequest req, boolean includeStackTrace) {
+		RequestAttributes requestAttributes = new ServletRequestAttributes(req);
+		return errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
+	}
+}
