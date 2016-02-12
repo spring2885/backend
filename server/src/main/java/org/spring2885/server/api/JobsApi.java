73aa6d4d6cc8a621690e0c24cfe610ee2df1df09
Rob Clevenger <rob@spring-2885.org>
2016-02-11 22:07:04 -0800

    Fixed definition of jobs api result type. Implemented stub jobs get api with sample data.

--- server/src/main/java/org/spring2885/server/api/JobsApi.java
+++ server/src/main/java/org/spring2885/server/api/JobsApi.java
@@ -2,9 +2,9 @@ package org.spring2885.server.api;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.spring2885.model.InlineResponse200;
 import org.spring2885.model.Job;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,70 +14,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-
 @Controller
-@RequestMapping(value = "/jobs", produces = {APPLICATION_JSON_VALUE})
-@Api(value = "/jobs", description = "the jobs API")
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringMVCServerCodegen", date = "2016-02-10T23:10:13.712-08:00")
+@RequestMapping(value = "/jobs", produces = { APPLICATION_JSON_VALUE })
 public class JobsApi {
-  
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ResponseEntity<List<Job>> jobsGet(@RequestParam(value = "size", required = true) Double size)
+			throws NotFoundException {
+		// do some magic!
+		List<Job> jobs = new ArrayList<Job>();
+		Job job = new Job();
+		job.setId(1L);
+		job.setJobType("full_time");
+		job.setDescription("This is a really good job.");
+		jobs.add(job);
+		return new ResponseEntity<List<Job>>(jobs, HttpStatus.OK);
+	}
 
-  @ApiOperation(value = "", notes = "Gets `Job` objects.\nOptional query param of **size** determines\nsize of returned array", response = InlineResponse200.class, responseContainer = "List")
-  @ApiResponses(value = { 
-    @ApiResponse(code = 200, message = "Successful response") })
-  @RequestMapping(value = "", 
-    
-    
-    method = RequestMethod.GET)
-  public ResponseEntity<List<InlineResponse200>> jobsGet(@ApiParam(value = "Size of array", required = true) @RequestParam(value = "size", required = true) Double size
+	@RequestMapping(value = "", method = RequestMethod.PUT)
+	public ResponseEntity<Void> jobsPut(@RequestBody Job job) throws NotFoundException {
+		// do some magic!
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
 
-
-)
-      throws NotFoundException {
-      // do some magic!
-      return new ResponseEntity<List<InlineResponse200>>(HttpStatus.OK);
-  }
-
-  
-
-  @ApiOperation(value = "", notes = "", response = Void.class)
-  @ApiResponses(value = { 
-    @ApiResponse(code = 200, message = "Updates the Job") })
-  @RequestMapping(value = "", 
-    
-    
-    method = RequestMethod.PUT)
-  public ResponseEntity<Void> jobsPut(
-
-@ApiParam(value = "The Job you want to update" ,required=true ) @RequestBody Job job
-)
-      throws NotFoundException {
-      // do some magic!
-      return new ResponseEntity<Void>(HttpStatus.OK);
-  }
-
-  
-
-  @ApiOperation(value = "", notes = "", response = Void.class)
-  @ApiResponses(value = { 
-    @ApiResponse(code = 200, message = "Make a new Job") })
-  @RequestMapping(value = "", 
-    
-    
-    method = RequestMethod.POST)
-  public ResponseEntity<Void> jobsPost(
-
-@ApiParam(value = "The Job you want to post" ,required=true ) @RequestBody Job job
-)
-      throws NotFoundException {
-      // do some magic!
-      return new ResponseEntity<Void>(HttpStatus.OK);
-  }
-
-  
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<Void> jobsPost(@RequestBody Job job) throws NotFoundException {
+		// do some magic!
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
 }
--- server/src/main/java/org/spring2885/server/api/PersonsApi.java
+++ server/src/main/java/org/spring2885/server/api/PersonsApi.java
@@ -14,72 +14,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-
 @Controller
-@RequestMapping(value = "/persons", produces = {APPLICATION_JSON_VALUE})
-@Api(value = "/persons", description = "the persons API")
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringMVCServerCodegen", date = "2016-02-10T23:10:13.712-08:00")
+@RequestMapping(value = "/persons", produces = { APPLICATION_JSON_VALUE })
 public class PersonsApi {
-  
 
-  @ApiOperation(value = "", notes = "Gets `person` objects.\nOptional query param of **size** determines\nsize of returned array", response = Person.class, responseContainer = "List")
-  @ApiResponses(value = { 
-    @ApiResponse(code = 200, message = "Successful response") })
-  	@RequestMapping(value = "", method = RequestMethod.GET)
-  public ResponseEntity<List<Person>> personsGet(
-		  @ApiParam(value = "Size of array", required = true) 
-		  @RequestParam(value = "size", required = true) Double size)
-      throws NotFoundException {
-      // do some magic!
-      // return new ResponseEntity<List<InlineResponse2001>>(HttpStatus.OK);
-	  List<Person> persons = new ArrayList<>();
-	  Person p = new Person();
-	  p.setFirstname("Charles");
-	  p.setLastname("Babbage");
-	  persons.add(p);
-      return new ResponseEntity<List<Person>>(persons, HttpStatus.OK);
-  }
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ResponseEntity<List<Person>> personsGet(@RequestParam(value = "size", required = true) Double size)
+			throws NotFoundException {
+		// TODO: Implement this.
+		List<Person> persons = new ArrayList<>();
+		Person p = new Person();
+		p.setFirstname("Charles");
+		p.setLastname("Babbage");
+		persons.add(p);
+		return new ResponseEntity<List<Person>>(persons, HttpStatus.OK);
+	}
 
-  
+	@RequestMapping(value = "", method = RequestMethod.PUT)
+	public ResponseEntity<Void> personsPut(@RequestBody Person person) throws NotFoundException {
+		// do some magic!
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
 
-  @ApiOperation(value = "", notes = "", response = Void.class)
-  @ApiResponses(value = { 
-    @ApiResponse(code = 200, message = "Updates the person") })
-  @RequestMapping(value = "", 
-    
-    
-    method = RequestMethod.PUT)
-  public ResponseEntity<Void> personsPut(
+	@RequestMapping(value = "",	method = RequestMethod.POST)
+	public ResponseEntity<Void> personsPost(@RequestBody Person person) throws NotFoundException {
+		// do some magic!
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
 
-@ApiParam(value = "The person you want to update" ,required=true ) @RequestBody Person person
-)
-      throws NotFoundException {
-      // do some magic!
-      return new ResponseEntity<Void>(HttpStatus.OK);
-  }
-
-  
-
-  @ApiOperation(value = "", notes = "", response = Void.class)
-  @ApiResponses(value = { 
-    @ApiResponse(code = 200, message = "Make a new person") })
-  @RequestMapping(value = "", 
-    
-    
-    method = RequestMethod.POST)
-  public ResponseEntity<Void> personsPost(
-
-@ApiParam(value = "The person you want to post" ,required=true ) @RequestBody Person person
-)
-      throws NotFoundException {
-      // do some magic!
-      return new ResponseEntity<Void>(HttpStatus.OK);
-  }
-
-  
 }
