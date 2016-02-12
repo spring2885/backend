e6657fee86a77845b50d75d835aa180a1d1b4142
Rob Clevenger <rob@spring-2885.org>
2016-02-11 20:49:54 -0800

    Add a generated model and api from swagger's spring-mvc codegen. This is just an example now of how to add new APIs, they are pretty easy.  For the model classes, please don't edit those since we'll be re-executing the codegen as we evolve the model.
    I'll figure out how to wire-up codegen for the model as part of the
    maven build and delete these eventually.

---
+++ server/src/main/java/org/spring2885/server/api/ApiException.java
@@ -0,0 +1,10 @@
+package org.spring2885.server.api;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringMVCServerCodegen", date = "2016-02-10T23:10:13.712-08:00")
+public class ApiException extends Exception{
+	private int code;
+	public ApiException (int code, String msg) {
+		super(msg);
+		this.code = code;
+	}
+}
