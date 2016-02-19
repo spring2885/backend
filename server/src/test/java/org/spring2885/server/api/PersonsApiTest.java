cb91056868bf16f93f4f22ceebc49edd1ecdff04
Rob Clevenger <rob@spring-2885.org>
2016-02-18 22:21:59 -0800

    added security for checking that only an admin user can delete others profiles. got the unit tests working with spring security.  There were MANY blogs on how to do this, and almost all of them are wrong or outdated since they didn't work with SecurityContextHolderAwareRequestWrapper.
    This gem helped:
    http://stackoverflow.com/questions/33524509/spring-mockmvc-spring-security-and-mockito

--- server/src/test/java/org/spring2885/server/api/PersonsApiTest.java
+++ server/src/test/java/org/spring2885/server/api/PersonsApiTest.java
@@ -1,29 +1,47 @@
 package org.spring2885.server.api;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.spring2885.server.db.model.DbPerson;
 import org.spring2885.server.db.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { TestConfig.class})
+@ContextConfiguration(classes = { TestConfig.class })
 @WebAppConfiguration
 public class PersonsApiTest {
     protected MockMvc mockMvc;
@@ -33,14 +51,17 @@ public class PersonsApiTest {
     
     @Autowired
     private PersonService personService;
-    
+
     @Before
     public void setup() {
         reset(personService);
-        mockMvc = webAppContextSetup(webappContext).build();
+        mockMvc = webAppContextSetup(webappContext)
+        		.apply(SecurityMockMvcConfigurers.springSecurity())
+        		.build();
     }
     
     @Test
+    @WithMockUser
     public void testPersons() throws Exception {
     	// Setup the expectations.
     	List<DbPerson> persons = new ArrayList<>();
@@ -66,6 +87,7 @@ public class PersonsApiTest {
      * Tests a {@code /profiles/:id} where {@code id} is found.
      */
     @Test
+    @WithMockUser
     public void testPersonsById() throws Exception {
     	// Setup the expectations.
     	DbPerson p = new DbPerson();
@@ -87,6 +109,7 @@ public class PersonsApiTest {
      * Tests a {@code /profiles/:id} where {@code id} is not found.
      */
     @Test
+    @WithMockUser
     public void testPersonsById_notFound() throws Exception {
     	// Setup the expectations.
     	when(personService.findById(21)).thenReturn(null);
@@ -102,17 +125,47 @@ public class PersonsApiTest {
 
 
     @Test
+    @WithMockUser(username="me@example.com",roles={"USER","ADMIN"})
     public void testDeletePersonsById() throws Exception {
     	// Setup the expectations.
-    	when(personService.delete(21)).thenReturn(true);
-    	verifyNoMoreInteractions(personService);
+    	DbPerson p = new DbPerson();
+    	p.setEmail("me@example.com");
+    	p.setId(4);
+    	when(personService.findByEmail(Mockito.anyString()))
+    		.thenReturn(Collections.singletonList(p));
+    	when(personService.delete(4)).thenReturn(true);
     	
-    	mockMvc.perform(delete("/api/v1/profiles/21")
-    			.accept(MediaType.APPLICATION_JSON))
+    	mockMvc.perform(delete("/api/v1/profiles/4"))
     			.andExpect(status().isOk());
     	
     	// Ensure PersonService#delete method was called since the result of our
     	// method is the same no matter what.
-    	verify(personService).delete(21);
+    	verify(personService).delete(4);
     }
+    
+    @Configuration
+    @EnableWebSecurity
+    @EnableWebMvc
+    static class TestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .usernameParameter("user")
+                    .passwordParameter("pass")
+                    .loginPage("/login")
+                 .and().csrf().disable();
+        }
+
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                .inMemoryAuthentication()
+                    .withUser("user").password("password").roles("USER");
+        }
+    }    
 }
