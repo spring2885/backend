853c7434910291cc7fb7ae7d60a4b38a28841135
Rob Clevenger <rob@spring-2885.org>
2016-02-19 21:20:44 -0800

    added /user

---
+++ server/src/main/java/org/spring2885/server/api/UserController.java
@@ -0,0 +1,14 @@
+package org.spring2885.server.api;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+      return user;
+    }
+}
