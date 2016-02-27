83a4d768392475cb841e6eaca7989e9ad37be7a7
Jenevieve <jenevieverantisi@spring-2885.org>
2016-02-26 19:20:06 -0800

    this is done now fixes https://github.com/spring2885/backend/issues/24

---
+++ server/src/test/java/org/spring2885/server/db/service/JobConverterTest.java
@@ -0,0 +1,31 @@
+package org.spring2885.server.db.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.spring2885.model.Job;
+import org.spring2885.server.db.model.DbJob;
+import org.spring2885.server.db.model.JobConverters;
+
+import com.google.common.base.Function;
+
+@RunWith(JUnit4.class)
+public class JobConverterTest {
+	public JobConverterTest(){}
+	
+	@Test
+	public void testFromDbToJson(){
+		Function<DbJob, Job> dtoj = JobConverters.fromDbToJson();
+		DbJob dbp = new DbJob();
+		Job p = new Job();
+		p = dtoj.apply(dbp);
+	}
+	
+	@Test
+	public void testFromJsonToDb(){
+		Function<Job, DbJob> jtod = JobConverters.fromJsonToDb();
+		Job p = new Job();
+		DbJob dbp = new DbJob();
+		dbp = jtod.apply(p);
+	}
+}
---
+++ server/src/test/java/org/spring2885/server/db/service/JobServiceTest.java
@@ -0,0 +1,137 @@
+package org.spring2885.server.db.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.spring2885.server.db.model.DbJob;
+import org.springframework.data.jpa.repository.JpaContext;
+
+import com.google.common.collect.Lists;
+
+@RunWith(JUnit4.class)
+public class JobServiceTest {
+	@Mock JobRepository repository;
+	private JobService service;
+
+    @Before public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+        service = new JobServiceImpl(repository);
+
+    	// return empty list by default
+    	when(repository.findByTitle(anyString())).thenReturn(Collections.emptyList());
+    }
+    
+    @Test
+    public void testFindAll() {
+    	DbJob p = new DbJob();
+    	when(repository.findAll()).thenReturn(Collections.singleton(p));
+
+    	List<DbJob> persons = Lists.newArrayList(service.findAll());
+    	assertEquals(1, persons.size());
+    	assertSame(p, persons.get(0));
+    }
+
+    @Test
+    public void testFindAll_none() {
+    	when(repository.findAll()).thenReturn(Collections.emptyList());
+
+    	List<DbJob> persons = Lists.newArrayList(service.findAll());
+    	assertEquals(0, persons.size());
+    }
+    
+    @Test
+    public void testFindByTitle() {
+    	DbJob p = new DbJob();
+    	p.setTitle("Hello");
+    	//p.setjob("Hello");
+    	when(repository.findByTitle("Hello")).thenReturn(Collections.singletonList(p));
+    	
+    	List<DbJob> persons = Lists.newArrayList(service.findByTitle("Hello"));
+    	assertEquals(1, persons.size());
+    	assertSame(p, persons.get(0));
+    }
+
+    @Test
+    public void testFindByEmail_None() {
+    	List<DbJob> jobs = Lists.newArrayList(service.findByTitle("World"));
+    	assertEquals(0, jobs.size());
+    }
+
+    @Test
+    public void testFindById() {
+    	DbJob expected = new DbJob();
+    	expected.setTitle("Hellow");
+    	when(repository.findOne(Long.valueOf(1234))).thenReturn(expected);
+    	
+    	DbJob actual = service.findById(1234);
+    	assertSame(expected, actual);
+    }
+    
+    @Test
+    public void testFindById_notFound() {
+    	DbJob actual = service.findById(1234);
+    	assertNull(actual);
+    }
+    
+    @Test
+    public void testExistsByTitle() {
+    	DbJob p = new DbJob();
+    	p.setTitle("Hello");
+    	when(repository.findByTitle("Hello")).thenReturn(Collections.singletonList(p));
+
+    	assertTrue(service.existsByTitle("Hello"));
+    }
+    
+    @Test
+    public void testExistsByEmail_doesNotExist() {
+    	assertFalse(service.existsByTitle("me@"));
+    }
+    
+    @Test
+    public void testDelete() {
+    	DbJob p = new DbJob();
+    	when(repository.findOne(Long.valueOf(21))).thenReturn(p);
+    	
+    	assertTrue(service.delete(21));
+    	verify(repository).delete(Long.valueOf(21));
+    }
+
+    @Test
+    public void testDelete_notFound() {
+    	DbJob p = new DbJob();
+    	when(repository.findOne(Long.valueOf(22))).thenReturn(p);
+    	
+    	assertFalse(service.delete(21));
+    	// Since a call to findOne(21) will return null by default, delete should
+    	// never be called. Let's verify that.
+    	verify(repository, never()).delete(Long.valueOf(21));
+    }
+
+    @Test
+    public void testSave() {
+    	DbJob p = new DbJob();
+    	DbJob expected = new DbJob();
+    	when(repository.save(same(p))).thenReturn(expected);
+    	
+    	DbJob actual = service.save(p);
+    	assertSame(expected, actual);
+    }
+}
+
