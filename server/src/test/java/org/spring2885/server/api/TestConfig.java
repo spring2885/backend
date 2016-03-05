427ac632c08695010b0a82e48604b6735a2bccf8
Rob Clevenger <rob@spring-2885.org>
2016-03-05 14:19:48 -0800

    since email is now a key for person, use it vs. the integer person_id in the json.  updated the DbNews model to use Person as a FK and not just as an opaque integer.

--- server/src/test/java/org/spring2885/server/api/TestConfig.java
+++ server/src/test/java/org/spring2885/server/api/TestConfig.java
@@ -2,6 +2,7 @@ package org.spring2885.server.api;
 
 import static org.mockito.Mockito.mock;
 
+import org.spring2885.server.db.model.NewsConverters;
 import org.spring2885.server.db.model.PersonConverters;
 import org.spring2885.server.db.service.JobService;
 import org.spring2885.server.db.service.JobTypeService;
@@ -29,7 +30,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
         "org.spring2885.server.api",
         "org.spring2885.server.model"
 })
-@Import(PersonConverters.class)
+@Import({ PersonConverters.class, NewsConverters.class })
 public class TestConfig {
 	@Bean
 	public PersonService personServiceMock() {
