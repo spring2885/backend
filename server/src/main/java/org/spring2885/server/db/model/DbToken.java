0b7a77a77560fa8c0ed86dcf7e312aee0418e463
Rob Clevenger <rob@spring-2885.org>
2016-03-06 07:32:27 -0800

    remove unused imports

--- server/src/main/java/org/spring2885/server/db/model/DbToken.java
+++ server/src/main/java/org/spring2885/server/db/model/DbToken.java
@@ -3,8 +3,6 @@ package org.spring2885.server.db.model;
 import java.sql.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
