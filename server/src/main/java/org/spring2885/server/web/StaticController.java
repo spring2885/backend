e4c96db8fa8caa012239836f55dc85df80b7f721
Rob Clevenger <rob@spring-2885.org>
2016-03-05 17:21:29 -0800

    Ensure ThymeLeaf stays configured. Moved the appname to a profile specific property so you can run in eclipse using ```--spring.profiles.active=dev``` in  your run configuration's program arguments and let application-dev.properties override the default ones. You can put your username/password in there for sending email using gmail.

--- server/src/main/java/org/spring2885/server/web/StaticController.java
+++ server/src/main/java/org/spring2885/server/web/StaticController.java
@@ -3,18 +3,22 @@ package org.spring2885.server.web;
 import java.util.Date;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StaticController {
+    @Value("${app.name}")
+    private String appName;
+    
     @RequestMapping("/home")
     public String home(Map<String, Object> model) {
 		model.put("message",
 			"This server is only for service APIs.\n"
             + "It is not intended to be accessed from a browser... \n"
             + "These are not the droids you are looking for... yada yada..");
-		model.put("title", "Spring 2885 Server");
+		model.put("title", appName);
 		model.put("date", new Date());
 		return "home";
     }
