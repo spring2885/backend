3f111f4283fc76155c88b7a2d18a30a4dfe1885b
Rob Clevenger <rob@spring-2885.org>
2016-03-05 16:39:02 -0800

    Added support for sending emails using the javamail api.

--- server/src/main/java/org/spring2885/server/api/PasswordApi.java
+++ server/src/main/java/org/spring2885/server/api/PasswordApi.java
@@ -2,7 +2,11 @@ package org.spring2885.server.api;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.slf4j.Logger;
@@ -12,7 +16,9 @@ import org.spring2885.server.db.model.DbPerson;
 import org.spring2885.server.db.model.DbToken;
 import org.spring2885.server.db.service.TokenService;
 import org.spring2885.server.db.service.person.PersonService;
+import org.spring2885.server.mail.Mailer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +34,10 @@ public class PasswordApi {
     @Autowired TokenService tokenService;
 	@Autowired PersonService personService;
 	@Autowired private PasswordEncoder passwordEncoder;
+	@Autowired private Mailer mailer;
+	@Value("${app.name}") private String appName;
+    @Value("${app.forgot.from.name}") private String fromName;
+    @Value("${app.forgot.reset.url}") private String resetUrl;
 	
 	@RequestMapping(value = "/forgot",method = RequestMethod.POST)
     public UUID personsResetToken(@RequestParam("email") String email) throws NotFoundException {
@@ -48,8 +58,20 @@ public class PasswordApi {
 		tokenData.setDateCreated(new java.sql.Date(System.currentTimeMillis()));
 		tokenService.save(tokenData);
 		
-        // TODO: Send Email
 		logger.info("Send Email to {} with token: {}", email, uuid);
+		Map<String, String> data = new HashMap<>();
+		data.put("name", p.getName());
+		data.put("app_name", appName);
+		data.put("reset_url", resetUrl);
+		data.put("from_name", fromName);
+		
+		try {
+            mailer.send(email, "forgot.txt", "Forgot Password.", data);
+        } catch (IOException | URISyntaxException e) {
+            // If we can't send the email there is no reason to give the UUID out.
+            logger.error("Error sending email to" + email, e);
+            throw new RuntimeException(e);
+        }
 
 		return uuid;
 	}
