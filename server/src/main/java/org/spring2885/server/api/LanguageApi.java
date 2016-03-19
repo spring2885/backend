161e77b8125e80ad6a343f51b76a245d5bcc0614
Dan <rujudan@gmail.com>
2016-03-18 18:04:26 -0700

    5 lines added to language.api. adding more "soon"

--- server/src/main/java/org/spring2885/server/api/LanguageApi.java
+++ server/src/main/java/org/spring2885/server/api/LanguageApi.java
@@ -1,5 +1,14 @@
 package org.spring2885.server.api;
 //PUT, POST, GET, DELETE, and GET{id}
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 public class LanguageApi {
 
+	@RequestMapping(method = RequestMethod.GET);
+	@RequestMapping(method = RequestMethod.PUT)
+	@RequestMapping(method = RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.DELETE)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET);
 }
