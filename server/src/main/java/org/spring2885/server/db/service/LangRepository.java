88e10934e9c14c6cb65b1869ecd0e9b04d4fe7fb
Dan <rujudan@gmail.com>
2016-03-20 00:28:29 -0700

    fixed langauge file structure

--- server/src/main/java/org/spring2885/server/db/service/LangRepository.java
+++
@@ -1,8 +0,0 @@
-package org.spring2885.server.db.service;
-
-import org.spring2885.server.db.model.DbLanguage;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LangRepository extends CrudRepository<DbLanguage,String> {
-	
-}
---
+++ server/src/test/java/org/spring2885/server/api/LanguageApiTest.java
@@ -0,0 +1,53 @@
+package org.spring2885.server.api;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.spring2885.model.News;
+import org.spring2885.server.db.model.DbLanguage;
+import org.spring2885.server.db.model.DbNews;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.google.common.collect.ImmutableList;
+
+public class LanguageApiTest {
+	protected MockMvc mockMvc;
+	
+	@Autowired protected WebApplicationContext webappContext;
+	
+	 private DbLanguage dbMy;
+	 private Lang my;
+	
+	  @Test
+	  @WithMockLang
+	    public void testNews() throws Exception {
+	    	// Setup the expectations.
+	    	when(LanguageServiceTest.findAll())
+	    		.thenReturn(ImmutableList.of(
+	    			createDbLang(5,  "Language #1"),
+	    			createDbLang(5,  "Langauge #2")));
+	    	
+	    	mockMvc.perform(get("/api/v1/Language")
+	    			.accept(MediaType.APPLICATION_JSON))
+	    			.andExpect(status().isOk())
+	    			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+	    			.andExpect(jsonPath("$[0].title", Matchers.is("Language #1")))
+	    			.andExpect(jsonPath("$[1].title", Matchers.is("Language #2")));
+	    }
+
+	static DbLanguage createDbLang(String code, String description) {
+    	DbLanguage n = new DbLanguage();
+    	n.setCode(code);
+    	n.setDescription(description);
+    	return n;
+    }
+}
