b029575852d71675b6398149ac69a217b935748a
Rob Clevenger <rob@spring-2885.org>
2016-04-01 14:58:24 -0700

    log calls to /user

--- server/src/main/java/org/spring2885/server/api/UserController.java
+++ server/src/main/java/org/spring2885/server/api/UserController.java
@@ -2,13 +2,18 @@ package org.spring2885.server.api;
 
 import java.security.Principal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @RequestMapping("/user")
     public Principal user(Principal user) {
-      return user;
+        logger.info("Handling /user with: {}", user.toString());
+        return user;
     }
 }
