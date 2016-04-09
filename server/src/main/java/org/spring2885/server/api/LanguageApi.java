bca3f1bd2e07ac192df595361ad6b971f8696ab5
Dan <rujudan@gmail.com>
2016-04-08 19:41:31 -0700

    updated  main/java/org/spring2885/model/Language.java and modified:   main/java/org/spring2885/server/api/LanguageApi.java

--- server/src/main/java/org/spring2885/server/api/LanguageApi.java
+++ server/src/main/java/org/spring2885/server/api/LanguageApi.java
@@ -1,5 +1,5 @@
 package org.spring2885.server.api;
-//PUT, POST, GET, DELETE, and GET{id}
+//PUT, POST, GET, DELETE, and GET{code}
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -7,12 +7,13 @@ import org.spring2885.model.Language;
 import org.spring2885.server.api.exceptions.NotFoundException;
 import org.spring2885.server.db.model.DbLanguage;
 import org.spring2885.server.db.model.LanguageConverters;
-import org.spring2885.server.db.model.NewsConverters;
 import org.spring2885.server.db.service.LanguageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,29 +22,46 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/v1/language", produces = { APPLICATION_JSON_VALUE })
 public class LanguageApi {
 
-    @Autowired
-    private LanguageService LanguageService;
-    
-    @Autowired
-    private LanguageConverters.JsonToDbConverter languageJsonToDb;
-    
-    @Autowired
-    private LanguageConverters.LanguageFromDbToJson dbToJsonConverter;
-	
+	@Autowired
+	private LanguageService LanguageService;
+
+	@Autowired
+	private LanguageConverters.JsonToDbConverter languageJsonToDb;
+
+	@Autowired
+	private LanguageConverters.LanguageFromDbToJson dbToJsonConverter;
+
 	@RequestMapping(value = "/{code}", method = RequestMethod.GET)
-	public ResponseEntity<Language> get(
-			@PathVariable("code") String code) throws NotFoundException {
+	public ResponseEntity<Language> get(@PathVariable("code") String code) throws NotFoundException {
 		DbLanguage o = LanguageService.findByCode(code);
 		if (o == null) {
-			// When adding test testLanguagesByCode_notFound, was getting a NullPointerException
+			// When adding test testLanguagesByCode_notFound, was getting a
+			// NullPointerException
 			// here, so needed to add this.
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<>(dbToJsonConverter.apply(o), HttpStatus.OK);
 
 	}
-//	@RequestMapping(method = RequestMethod.PUT)
-//	@RequestMapping(method = RequestMethod.POST)
-//	@RequestMapping(method = RequestMethod.DELETE)
-//	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/{code}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> put(@PathVariable("code") String code, @RequestBody Language lang,
+			SecurityContextHolderAwareRequestWrapper request) throws NotFoundException {
+		DbLanguage db = LanguageService.findByCode(code);
+		if (db == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		DbLanguage updatedDbLanguageService = languageJsonToDb.apply(db, lang);
+		LanguageService.save(updatedDbLanguageService);
+
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	@RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> newsPost(@RequestBody Language lang) throws NotFoundException {
+		
+        DbLanguage updatedDbLanguage = languageJsonToDb.apply(new DbLanguage(), lang);
+		LanguageService.save(updatedDbLanguage);
+		
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
 }
